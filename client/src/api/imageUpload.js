1c578d249835e5f3f2e02265385fe7b6f1a0498f
Mahbub2001 <imahbubturza@gmail.com>
2022-11-22 02:19:53 +0600

    update

--- client/src/api/imageUpload.js
+++ client/src/api/imageUpload.js
@@ -3,7 +3,7 @@ export const imageUpload = async image => {
   if (image) {
     const formData = new FormData()
     formData.append('image', image)
-    const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMGBB_KEY}`
+    const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGE_API}`
 
     const response = await fetch(url, {
       method: 'POST',
